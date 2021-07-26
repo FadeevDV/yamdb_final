76a49aa73816b26a2c537064db9f6009bb84d5b7
FadeevDV <dfadeev.zld@yandex.ru>
2021-07-26 19:05:33 +0300

    commit_34

--- titles/views.py
+++ titles/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.response import Response
-
 from titles.filters import TitleFilter
-from titles.models import Category, Genre, Title
-from titles.serializers import (CategorySerializer, GenreSerializer,
-                                TitleSerializer_NOTSAFE, TitleSerializer_SAFE)
 from users.permissions import IsAdminOrReadOnly
+
+from titles.models import Category, Genre, Title
+from titles.serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleSerializer_SAFE,
+    TitleSerializer_NOTSAFE
+)
 
 
 class CategoryViewSet(
